9dc0239b7d61afce83d705dffeea4544ad0104fc
Eraldo Sinicio <eraldosinicio@gmail.com>
2024-12-28 18:22:13 -0300

    chore: create short url page

---
+++ src/pages/form/index.js
@@ -0,0 +1,58 @@
+"use client";
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const PUBLIC_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export function FormComponent() {
+  const [url, setUrl] = useState("");
+  const [shortedUrl, setShortedUrl] = useState(null);
+
+  async function shorterUrl(url) {
+    try {
+      const result = await fetch(PUBLIC_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ original: url }),
+      });
+
+      if (!result.ok) {
+        throw new Error("Erro na requisição");
+      }
+
+      const data = await result.json();
+      setShortedUrl(data[0]);
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  }
+
+  return (
+    <>
+      <img
+        width={200}
+        src="logo.webp"
+        className="rounded-2xl transition-transform duration-300 ease-in-out hover:scale-110"
+      />
+
+      <Input
+        onChange={({ target }) => setUrl(target.value)}
+        placeholder="http://www.google.com"
+      />
+
+      <Button onClick={() => shorterUrl(url)}>Encurtar</Button>
+
+      {shortedUrl && (
+        <div>
+          Seu link encurtado: {PUBLIC_URL}/{shortedUrl.short}
+        </div>
+      )}
+    </>
+  );
+}
